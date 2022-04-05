4a6f798b37437afea71377123d5dd613bd4e7e40
shortpupper <78228290+shortpupper@users.noreply.github.com>
2022-04-04 21:31:25 -0500

    Update snack.py
    s

--- snack.py
+++ snack.py
@@ -19,40 +19,49 @@ so make it so that it could clear the screen or get rid of the last pos
 def main(stdscr):
  doesDebug = False
  my, mx = stdscr.getmaxyx()
- py, px = 10, 10 #also starting pos
+ wallSizeY, wallSizeX = my, mx
+ py, px = int(wallSizeY/2), int(wallSizeX/2) #also starting pos
  ogpy, ogpx = py, px
  pos = []
  wallTiles = []
  k,l = 6, 6
  extraApples = 0
  apples = [(k, l), (3, 7), (5, 9)]
- length = 2
+ length = 5
  wall = True
  wallTileCar = "░▒▓█"[1]
  oglenght = length
+ canPlaceAtpplesInSnack = False
  lastArrowKey = None # 0: down, 1: up, 2: left, 3: right
  curses.init_pair(10, curses.COLOR_RED, curses.COLOR_BLACK)
+ curses.init_pair(11, curses.COLOR_YELLOW, curses.COLOR_WHITE)
  stdscr.clear()
  kko = 0
  tst = 1
  doesdie = True
+ autoSnack = True
+ def closestAtpple():
+  if len(apples) > 0:
+   pass
+  return (0, 0)
+  pass
  def makeWall():
   #var for the position of the wall
   wallYX = []
   #makes the bottom wall
-  for i in range(mx-1):
-   stdscr.addstr(my-1, i, wallTileCar)
-   wallYX += [(my-1, i)]
+  for i in range(wallSizeX-1):
+   stdscr.addstr(wallSizeY-1, i, wallTileCar)
+   wallYX += [(wallSizeY-1, i)]
   #makes the top wall
-  for i in range(mx-1):
+  for i in range(wallSizeX-1):
    stdscr.addstr(0, i, wallTileCar)
    wallYX += [(0, i)]
   #makes the left wall
-  for i in range(my-1):
-   stdscr.addstr(i, mx-1, wallTileCar)
-   wallYX += [(i, mx-1)]
+  for i in range(wallSizeY-1):
+   stdscr.addstr(i, wallSizeX-1, wallTileCar)
+   wallYX += [(i, wallSizeX-1)]
   #makes the right wall
-  for i in range(my):
+  for i in range(wallSizeY):
    stdscr.addstr(i, 0, wallTileCar)
    wallYX += [(i, 0)]
   return wallYX
@@ -76,13 +85,20 @@ def main(stdscr):
   if li < lo: return True
   return False
  for x in range(extraApples):
-  k, l = rand(1, my-1), rand(1, mx-1)
-  apples.append((k, l))
+  if not canPlaceAtpplesInSnack:
+   while True:
+    k, l = rand(1, wallSizeY-1), rand(1, wallSizeX-1)
+    if (k, l) not in pos: 
+     apples.append((k, l))
+     break
+  else: 
+   k, l = rand(1, wallSizeY-1), rand(1, wallSizeX-1)
+   apples.append((k, l))
  if wall: wallTiles = makeWall()
  while True:
   stdscr.addstr(py, px, "%")
   stdscr.addstr(5, 5, str(kko)) if doesDebug else None
-  if len(pos) > 0: stdscr.addstr(pos[-1][0], pos[-1][1], "#")
+  if len(pos) > 0: stdscr.addstr(pos[-1][0], pos[-1][1], "+")#π
   pos.append((py, px))# if tst != 1 or len(pos) <= 1 else None
   if doesSnackHitWall(): kko = 1
   if doesSnackHitSnake() and doesdie: kko = 1
@@ -94,8 +110,15 @@ def main(stdscr):
   if (pos[-1][0], pos[-1][1]) in apples:
    length += 1
    apples.pop(apples.index((pos[-1][0], pos[-1][1])))
-   k, l = rand(1, my-1), rand(1, mx-1)
-   apples.append((k, l))
+   if not canPlaceAtpplesInSnack:
+    while True:
+     k, l = rand(1, wallSizeY-1), rand(1, wallSizeX-1)
+     if (k, l) not in pos: 
+      apples.append((k, l))
+      break
+   else: 
+    k, l = rand(1, wallSizeY-1), rand(1, wallSizeX-1)
+    apples.append((k, l))
    #for e in range(5):#len(apples)
     #k, l = rand(1, my-1), rand(1, mx-1)
     #apples.append((k, l))
@@ -107,8 +130,8 @@ def main(stdscr):
    stdscr.addstr(my-4, 2, str([x for x in range(4) if x not in hardCollide()]))
    stdscr.addstr(my-5, 2, str([str(x).replace("0", "up").replace("1", "down").replace("2", "right").replace("3", "left") for x in range(4) if x not in hardCollide()]))
    stdscr.addstr(my-6, 2, str(wallTiles[:4]))
+  if autoSnack: curses.halfdelay(1)
   c = stdscr.getch()
-  #curses.halfdelay(10)
   # y, x, string
   if doesDebug:
    stdscr.addstr(my-1, 2, str(c))
@@ -132,7 +155,7 @@ def main(stdscr):
    lastArrowKey = 3
   elif c == ord('|'): break
   elif c == ord('p'):
-   k, l = rand(1, my-1), rand(1, mx-1)
+   k, l = rand(1, wallSizeY-1), rand(1, wallSizeX-1)
    apples.append((k, l))
   else:
    if kko == 0 and c != ord("b"):
@@ -151,19 +174,19 @@ def main(stdscr):
     elif r == 3: #right
      px -= 1
      lastArrowKey = 3
-   elif c == ord("b"):stdscr.addstr(my-4, 4, str(0 if (py, px) in pos else 1))
+   elif c == ord("b"):stdscr.addstr(wallSizeY-4, 4, str(0 if (py, px) in pos else 1))
   #if kko == 1 and c == 259:
   # c2 = stdscr.getch()
   # if c2 == 260:
   #  print("Window")
-  if c == 10 and kko == 1:
+  if c == 10 or autoSnack and kko == 1:
    stdscr.clear()
    if wall: wallTiles = makeWall()
    kko = 0
    pos = []
    length = oglenght
    py, px = ogpy, ogpx
-  if kko == 1:stdscr.addstr(int(my/2), int(mx/2), str("You Died"))
+  if kko == 1:stdscr.addstr(int(wallSizeY/2), int(wallSizeX/2), str("You Died"))
   stdscr.refresh()
  stdscr.getkey()
 wrapper(main)
